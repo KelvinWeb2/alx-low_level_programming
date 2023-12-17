9a28155879da167b4357103a96023fc7764fd3c7
root <kelvinimere@gmail.com>
2023-12-17 02:07:39 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char kul, k, v;
+
+	k = 'k';
+	v = 'v';
+
+	for (kul = 'a'; kul <= 'z'; kul++)
+	{
+		if (kul != k && kul != q)
+		{
+			putchar(kul);
+		}
+	}
+		putchar('\n');
+		return (0);
+}
