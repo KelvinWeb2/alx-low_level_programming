ee37daea39bdf79d5b2235890391af9c7188d0a4
root <kelvinimere@gmail.com>
2023-12-19 15:11:56 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print alphabet 10x
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	char k;
+	int i = 0;
+
+	while (i <= 0)
+	{
+		for (k = 'a'; k <= 'z'; k++)
+		{
+			_putchar(k);
+		}
+		_putchar('\n');
+		i++;
+	}
+}
