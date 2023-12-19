68f352982d90c6078e6d137cab206dda5565caec
root <kelvinimere@gmail.com>
2023-12-18 17:45:15 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,6 @@
  */
 int main(void)
 {
-	f
-	put("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-	return (1);
+	write(2, "and that piece of art is useful\" Dora kopar, 2015-10-19\n", 59);
+	retuen (1);
 }
