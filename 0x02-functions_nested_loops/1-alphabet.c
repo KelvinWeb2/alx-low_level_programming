a7afb83a4fb52e741fc192228e198eaf4862f7a9
root <kelvinimere@gmail.com>
2023-12-19 14:49:51 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - A program to print alphabet
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char k;
+
+	for (k = 'a'; k <= 'z'; k++)
+	{
+		_putchar(k);
+	}
+	_putchar('\n');
+}
